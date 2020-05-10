2a09990701b58dd866734991ab6ef2ec89308667
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2020-05-10 12:12:23 +0300

    Add initial array shuffling

--- src/Game/index.js
+++ src/Game/index.js
@@ -1,7 +1,9 @@
+import shuffle from 'Utils/shuffle';
+
 function getInitialState() {
   return {
     boardSize: 4,
-    tiles: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, -1, 14],
+    tiles: shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, -1]),
   };
 }
 
